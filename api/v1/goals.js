d9f0c2aba3f9418dd33eb2febfb549a81a59f5c4
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2017-03-11 16:31:03 -0700

    crud goal

--- api/v1/goals.js
+++ api/v1/goals.js
@@ -18,9 +18,38 @@ router.post('/', (req, res) => {
     finish_date: req.body.finish_date
   }).save()
   .then((saved) => {
-    console.log(saved);
     res.json({ saved });
   })
+})
+
+router.get('/:id', (req, res) => {
+  Goal.where('id', req.params.id).fetch()
+    .then((goal)=> {
+      res.json({ goal });
+    })
+})
+
+router.put('/:id', (req,res) => {
+  Goal.where('id', req.params.id).fetch()
+    .then((goal)=> {
+      goal.save({
+        exercise_name: req.body.exercise_name,
+        reps: req.body.reps,
+        load: req.body.load,
+        finish_date: req.body.finish_date
+      }).then((updated)=>{
+        console.log(updated);
+        res.json({ updated });
+      })
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  Goal.where('id', req.params.id).destroy()
+    .then((destroyed)=> {
+      console.log(destroyed);
+      res.json({ destroyed })
+    })
 })
 
 module.exports = router;
