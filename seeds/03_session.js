8f3ac62b469ea40598eedfd0fb18f3f791a5fa97
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2017-03-22 12:52:00 -0600

    reseeded again

--- seeds/03_session.js
+++ seeds/03_session.js
@@ -7,10 +7,10 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('session').insert([
         {id: 1, date: '03/15/2017', duration: 30, goal_id:2 },
-        {id: 2, date: '03/15/2017', duration: 20, goal_id:1 },
-        {id: 3, date: '03/15/2017', duration: 45, goal_id:3 },
-        {id: 4, date: '03/15/2017', duration: 60, goal_id:2 },
-        {id: 5, date: '03/15/2017', duration: 90, goal_id:2 }
+        {id: 2, date: '03/16/2017', duration: 20, goal_id:1 },
+        {id: 3, date: '03/17/2017', duration: 45, goal_id:3 },
+        {id: 4, date: '03/18/2017', duration: 60, goal_id:2 },
+        {id: 5, date: '03/19/2017', duration: 90, goal_id:2 }
 
       ]);
     });
